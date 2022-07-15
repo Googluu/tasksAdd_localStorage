5896c11e7d7f6b17d51e88c623230bfeddeded82
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-15 08:47:14 -0500

    :memo: add new task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
+import {useState} from 'react';
 import './App.css';
 import {TaskCreator} from './components/TaskCreator'
 
 function App() {
 
+  const [tasksItems, setTasksItems] = useState([
+    {
+      name: 'mi primer tarea',
+      done: false
+    },
+    {
+      name: 'mi segunda tarea',
+      done: false
+    },
+    {
+      name: 'mi tercer tarea',
+      done: false
+    },
+  ])
+
+  function createNewTask(taskName) {
+    if (!tasksItems.find(task => task.name === taskName)) {
+    setTasksItems([
+      ...tasksItems,
+      {name: taskName, donde: false}
+    ])
+    }
+  }
+
   return (
     <div className="App">
+      <TaskCreator createNewTask={createNewTask} />
 
-      <TaskCreator />
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            tasksItems.map(task => (
+              <tr key={task.name}>
+                <td>
+                  {task.name}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
 
     </div>
   );
